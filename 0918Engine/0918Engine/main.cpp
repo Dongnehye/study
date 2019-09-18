b3954800d03840ce1343745fb023b48412398ea4
Dongnehye <cmmn123@gmail.com>
2019-09-18 17:58:14 +0900

    09/18 엔진 lib 링커 TCP/IP server client

---
+++ 0918Engine/0918Engine/main.cpp
@@ -0,0 +1,72 @@
+#pragma comment(lib, "msimg32.lib")
+#include <windows.h>
+#include "Text.h"
+
+#define ID_EDIT 100
+#define ID_SAVE 200
+#define ID_LOAD 300
+#define ID_GROUPBOX 400
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+char g_szClassName[256] = "Hello World!!";
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(LTGRAY_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = g_szClassName;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(g_szClassName, g_szClassName, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+		1060, 720, NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0))
+	{
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+
+	Text t;
+
+	switch (iMessage)
+	{
+	case WM_CREATE:
+
+		return 0;
+	case WM_TIMER:
+		InvalidateRect(hWnd, NULL, false);
+		return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		EndPaint(hWnd, &ps);
+
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
